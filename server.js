e1616d0e74b1b990ce1cb1ee5f24ee150b94c2b2
R.James <rjimmy2512@gmail.com>
2019-12-08 22:08:15 -0800

    lab 13 files and codebase

--- server.js
+++ server.js
@@ -1,179 +1,120 @@
-'use strict'
+'use strict';
 
 //psql -d app_name -f schema.sql where app_name is your app
 //heroku pg:psql --app app_name < schema.sql
 
 const express = require('express');
-const app = express();
 const superagent = require('superagent');
 require('dotenv').config();
-const methodOverride = require('method-override');
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL);
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-let shelfCategories = [];
+// Tells express to use the built-in rules for ejs
+app.set('view engine', 'ejs');
 
-//tells express to use the built-in rules for ejs 
-app.set('view engine', 'ejs');  
+// Tells express to find static files (like css) in the public dir
+app.use('/public', express.static('public'));
 
-//tells express to find static files (like css) in the public dir
-app.use(express.static('public')); 
-
-//tells express to read all incoming body info (from the Books api)
+// Tells express to read all incoming body info (from the Books api)
 app.use(express.urlencoded({extended:true}));
 
-app.use(methodOverride( (request, response) => {
-  if(request.body && typeof request.body === 'object' && '_method' in request.body) {
-    console.log('method override: ', request.body); 
-    let method = request.body._method;
-    delete request.body._method;
-    return method;
-  }
-}));
-
-//connect to the SQL server; if unsuccessful, throw an error
 client.connect();
 client.on('error', err => console.error(err));
 
-//Routes
-app.get('/', buildIndex);
-app.get('/search', newSearch);
-app.post('/searchresults', searchAPI);
-app.get('/books/:book_id', getOneBook);
-app.get('/add', (req, res) => res.render('pages/editbook', {shelves: shelfCategories}));
+
+//ROUTES
+app.get('/', getBooks);
+app.get('/new', newSearch);
 app.post('/add', addNewBook);
-
-app.put('/books/:book_id', updateBook);
-
-app.delete('/books/:id', deleteBook);
-
-function updateBook(req, res) {
-  let sql = 'UPDATE books SET image=$1, title=$2, authors=$3, description=$4, shelf=$5, isbn=$6 WHERE id=$7;';
-  let { image, title, authors, description, shelf, isbn } = req.body;
-  let values = [image, title, [authors], description, [shelf], isbn, req.params.book_id];
-  client.query(sql, values) 
-  .then(res.redirect(`/book/${req.params.book_id}`));
-}
-
-//Helper Functions
-async function addNewBook(req,res){
-  let r = req.body;
-  let sql = 'INSERT INTO books(image,title,authors,description,shelf,isbn) VALUES($1,$2, $3, $4, $5, $6) RETURNING id;';
-  let values = [r.image, r.title, [r.authors], r.description, [r.shelf], r.isbn];
-  let result = await client.query(sql, values)
-  .then( result => {
-      if(result.rowCount > 0){
-        console.log('result.rows[0].id: ', result.rows[0].id);
-          res.redirect((`/book/${result.rows[0].id}`));
-      }
-  })
-}
-
-async function getOneBook(req,res){
-  let sql = 'SELECT * FROM books WHERE id=$1;'
-  let result = await client.query(sql,[req.params.book_id]);
-  res.render('searches/new', {searchResults:result.rows, route: '/book', shelves: shelfCategories});
-}
+app.post('/searches', createSearch);
+app.get('/add', (req, res) => {
+  res.render('pages/add');
+});
+app.get('/books/:book_id', getOneBook);
+app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
 
-async function getShelfCategories(req,res){
-  let sql = 'SELECT DISTINCT shelf FROM books;';
-  let result = await client.query(sql);
-  let resultRows = result.rows;
-  shelfCategories.length = 0;
-  
-  resultRows.forEach( (row, idx) => {
-    row.shelf.forEach( shelf => {
-      if(!(shelf.includes(','))){
-        shelfCategories.push(shelf);
-      }
-      if(shelf.includes(',')){
-        shelf = shelf.split(', ' || ',');
-        shelf.forEach( category => {
-          shelfCategories.push(category);
-        })
-      }
-    })
-  });
-}
 
-async function buildIndex(req,res){
+// Helper Functions
+function getBooks(req, res) {
   let sql = 'SELECT * FROM books;';
-  let result = await client.query(sql);
-  console.log('result.rows from buildIndex: ', result.rows);
-
-  getShelfCategories();
-
-  res.render('pages/index', {searchResults:result, route: '/'});
+  return client.query(sql)
+    .then(response => {
+      if (response.rowCount > 0) {
+        res.render('pages/index', { allBooks: response.rows });
+      }
+    });
 }
 
-function newSearch(req, res){ //renders the search.ejs file in pages dir 
-  res.render('searches/search')
+function newSearch(req, res) {
+  res.render('pages/new');
 }
 
-async function searchAPI(req, res){
-  let url = 'https://www.googleapis.com/books/v1/volumes?q=';  //this is not the full URL
-  //these if statements determine the rest of the URL
-  if(req.body.search[1] === 'title' ) {url += `intitle:${req.body.search[0]}`;}
-  if(req.body.search[1] === 'author' ) {url += `inauthor:${req.body.search[0]}`;}
-  try{
-    //wait for the result of the API call
-    let result = await superagent.get(url);
-    console.log('result.body.items: ', result.body.items);
-    console.log('********************~~~~~~~~~~~~~~~~~*********************~~~~~~~~~~~~~~~~~*********************~~~~~~~~~~~~~~~~~*********************');
-    console.log('result.body.items[3].volumeInfo.categories: ', result.body.items[0].volumeInfo.categories);
-
-    //instantiate book objects, and assign those objects to a new array
-    let bookArray = result.body.items.map(bookResult => new Book(bookResult.volumeInfo));
-    //console log the first book to make sure schema is ok
-    console.log('bookArray[0]): ', bookArray[0]);
-
-    //pass the array of book objects back to the response
-    res.render('searches/searchresults', {searchResults:bookArray, route: '/searchresults', shelves: shelfCategories});
-  }
-  catch{
-    //if something goes wrong, say something.
-    // if(result.body.items.length === 0 ){
-      errorHandler(`No books with the ${req.body.search[1]} ${req.body.search[0]} was found.`, req, res);
-    // }
-  }
+function addNewBook(req, res) {
+  let r = req.body;
+  let sql = 'INSERT INTO books(authors, title, isbn,image_url,description_book,bookshelf) VALUES($1, $2, $3, $4, $5, $6) RETURNING id';
+  let values = [r.authors, r.title, r.isbn, r.image_url, r.description_book, r.bookshelf];
+  client.query(sql, values)
+    .then(result => {
+      if (result.rowCount > 0) {
+        res.redirect('/');
+      }
+    });
 }
 
-function deleteBook(req, res) {
-  console.log('$1: ');
-  let sql = 'DELETE FROM books WHERE id=$1;';
-  console.log('+++++++++++______________++++++++++++DELETE+++++++++++______________++++++++++++');
-  console.log('req.body.id: ', req.body.id);
-  let values = [parseInt(req.body.id)];
-  console.log('req.body.id: ', values);
-  console.log('+++++++++++______________++++++++++++DELETE+++++++++++______________++++++++++++');
-  return client.query(sql, values)
-    .then(res.redirect('/'));
+function createSearch(req, res) {
+  console.log(req.body.search);
+  let url = 'https://www.googleapis.com/bcooks/v1/volumes?q=';
+  if (req.body.search[1] === 'title') { url += `intitle:${req.body.search[0]}`; }
+  if (req.body.search[1] === 'author') { url += `inauthor:${req.body.search[0]}`;}
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => res.render('pages/show', { searchResults: results }));
+}
+
+function getOneBook(req, res) {
+  let sql = 'SELECT * FROM books WHERE id=$1;';
+  return client.query(sql, [req.params.book_id])
+    .then(result => {
+      if (result.rowCount > 0) {
+        console.log('result', result.rows);
+        res.render('pages/searches/details', { bookDetail: result.rows });
+      }
+    });
 }
 
 //Book constructor
 function Book(info){
-  this.image = info.imageLinks.thumbnail;
   this.title = info.title || 'No title available';
-  this.authors = info.authors || ['No authors listed'];
-  this.description = info.description || 'No description available';
-  if(info.industryIdentifiers.length > 1){
-    this.isbn = info.industryIdentifiers[1].identifier;
-  }
-  if(info.industryIdentifiers.length === 1){
-    this.isbn = info.industryIdentifiers[0].identifier;
-  }
-  this.shelf = info.categories || 'No bookshelf specified';
+  this.authors = info.authors || 'No author';
+  this.description = info.description || 'No Description';
+  this.img = info.imageLinks.thumbnail;
+  this.publishDate = info.publishedDate;
+  this.isbn = info.type || 'No ISBN available';
+  this.identifier = info.identifier;
 }
 
-//DON'T FORGET TO HANDLE ERRORS!!!!
+function menu() {
+  var x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
 
+
+// Handling Errors!!!!
 app.get('*', (req, res) => res.status(404).send('This route does not exist'));
 
 function errorHandler(error, req, res) {
-  res.status(500).send(error);
-  // res.render('pages/error');
+  res.render('pages/error');
 }
 
+
+// Turning on server
 app.listen(PORT, () => console.log(`server up on ${PORT}`));
